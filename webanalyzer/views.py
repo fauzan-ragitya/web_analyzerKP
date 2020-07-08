bc57a3a38f06a7820536348ac4d1c8faa1a45d76
fauzan-ragitya <57058232+fauzan-ragitya@users.noreply.github.com>
2020-07-08 16:09:23 +0700

    Update views.py

--- webanalyzer/views.py
+++ webanalyzer/views.py
@@ -112,15 +112,3 @@ def session_average_analisis(request):
         "labels" : labels , 
         "data" : data ,
     })
-
-##Testing fafa
-from django.http import JsonResponse
-from django.core import serializers
-
-def dashboard_with_pivot(request):
-    return render(request, 'dashboard_with_pivot.html', {})
-
-def pivot_data(request):
-    dataset = Session.objects.all()
-    data = serializers.serialize('json', dataset)
-    return JsonResponse(data, safe=False)

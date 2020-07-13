2b85b4350778f09aaa5c73080df0d928ae3c40bb
fauzan-ragitya <57058232+fauzan-ragitya@users.noreply.github.com>
2020-07-13 13:33:45 +0700

    Update urls.py

--- webanalyzer/urls.py
+++ webanalyzer/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('pie-chart/', views.pie_chart , name= 'pie-chart'),
     path('session_analisis' , views.session_average_analisis , name = 'session_analisis'),
     path('fingerprint/<int:heatmap_id>/', views.visualize, name='visualize'),
+    path('session_daily_analisis' , views.session_daily_analisis , name = 'session_daily_analisis'),
 ]
     
